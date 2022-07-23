7eb90f1eab42a58b43dea741b2df966c7e792fc1
dseum22 <dseum22@gmail.com>
2022-07-23 20:59:06 +0900

    Basic home items placed

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import styles from './Home.module.css';
-import headImage from './images/home-head.png';
+import headImage from './assets/images/home-head.png';
 import Navigator from './Navigator';
 
 function Home() {
   return (
     <main className={styles.main}>
       <Navigator />
-      <img className={styles.headImage} src={headImage} alt="" />
+      <div className={styles.headImageContainer}>
+        <img className={styles.headImage} src={headImage} alt="" />
+      </div>
+      <div className={styles.stripes}>
+        <span />
+        <span />
+        <span />
+      </div>
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>
+          <span>HackHarvard</span>
+          <span>2022</span>
+        </h1>
+      </div>
+      <div className={styles.banner}>HackHarvard 2022! Apply today!</div>
     </main>
   );
 }
