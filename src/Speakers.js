c7b546d3bc02c7720f56890f166cc4598b9c8cfe
Nathan Li <moncskoli@gmail.com>
2022-07-24 15:47:58 -0400

    simple speakers component working

--- src/Speakers.js
+++ src/Speakers.js
@@ -1,93 +1,107 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+// import Carousel from 'react-multi-carousel';
+// import 'react-multi-carousel/lib/styles.css';
 import styles from './styles/Speakers.module.css';
 import speakersLines from './assets/images/speakers-lines.png';
 import speakersAngles from './assets/images/speakers-angles.png';
 import img1 from './assets/images/1.jpg';
 import img2 from './assets/images/2.jpg';
 import img3 from './assets/images/3.jpg';
-import img4 from './assets/images/4.jpg';
-import img5 from './assets/images/5.jpg';
-import img6 from './assets/images/6.jpg';
-import img7 from './assets/images/7.jpg';
-import img8 from './assets/images/8.jpg';
+// import img4 from './assets/images/4.jpg';
+// import img5 from './assets/images/5.jpg';
+// import img6 from './assets/images/6.jpg';
+// import img7 from './assets/images/7.jpg';
+// import img8 from './assets/images/8.jpg';
+
+// const responsive = {
+//   desktop: {
+//     breakpoint: { max: 3000, min: 1024 },
+//     items: 3,
+//     slidesToSlide: 3, // optional, default to 1.
+//   },
+//   tablet: {
+//     breakpoint: { max: 1024, min: 464 },
+//     items: 2,
+//     slidesToSlide: 2, // optional, default to 1.
+//   },
+//   mobile: {
+//     breakpoint: { max: 464, min: 0 },
+//     items: 1,
+//     slidesToSlide: 1, // optional, default to 1.
+//   },
+// };
 
 function Speakers() {
   return (
     <main className={styles.main}>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-          crossOrigin="anonymous"
-        />
-        <script src="assets/js/jquery-3.3.1.min.js" />
-        <script src="assets/js/jquery-migrate-3.0.0.min.js" />
-        <script
-          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
-          integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
-          crossOrigin="anonymous"
-        />
-        <script
-          src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
-          integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
-          crossOrigin="anonymous"
-        />
-        <script src="./Speakers.module.js" />
-      </head>
       <h1 className={styles.titleText}>SPEAKERS</h1>
       <img className={styles.speakersLines} src={speakersLines} alt="" />
       <img className={styles.speakersAngles} src={speakersAngles} alt="" />
-      <div
-        id="carousel-example"
-        className="carousel slide"
-        data-ride="carousel"
-      >
-        <div className="carousel-inner row w-100 mx-auto" role="listbox">
-          <div className="carousel-item col-12 col-sm-6 col-md-4 col-lg-3 active">
-            <img src={img1} className="img-fluid mx-auto d-block" alt="img1" />
-          </div>
-          <div className="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
-            <img src={img2} className="img-fluid mx-auto d-block" alt="img2" />
-          </div>
-          <div className="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
-            <img src={img3} className="img-fluid mx-auto d-block" alt="img3" />
-          </div>
-          <div className="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
-            <img src={img4} className="img-fluid mx-auto d-block" alt="img4" />
-          </div>
-          <div className="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
-            <img src={img5} className="img-fluid mx-auto d-block" alt="img5" />
-          </div>
-          <div className="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
-            <img src={img6} className="img-fluid mx-auto d-block" alt="img6" />
-          </div>
-          <div className="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
-            <img src={img7} className="img-fluid mx-auto d-block" alt="img7" />
-          </div>
-          <div className="carousel-item col-12 col-sm-6 col-md-4 col-lg-3">
-            <img src={img8} className="img-fluid mx-auto d-block" alt="img8" />
-          </div>
-        </div>
-        <a
-          className="carousel-control-prev"
-          href="#carousel-example"
-          role="button"
-          data-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#carousel-example"
-          role="button"
-          data-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true" />
-          <span className="sr-only">Next</span>
-        </a>
+      <div className={styles.speakerCarousel}>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={img1}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={img2}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={img3}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
+      {/* <Carousel
+        swipeable={false}
+        draggable={false}
+        showDots
+        responsive={responsive}
+        ssr // means to render carousel on server-side.
+        infinite
+        autoPlay={this.props.deviceType !== "mobile" ? true : false}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        deviceType={this.props.deviceType}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        <div><img src={img1} alt="img1" /></div>
+        <div><img src={img2} alt="img2" /></div>
+        <div><img src={img3} alt="img3" /></div>
+        <div><img src={img4} alt="img4" /></div>
+      </Carousel> */}
     </main>
   );
 }
