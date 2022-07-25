5c332cb2a2ccd957c826f68b3ef9361455eb0672
MKJM2 <mailformmk@gmail.com>
2022-07-25 22:26:49 +0200

    Adding styles to the Tracks website

--- src/Tracks.js
+++ src/Tracks.js
@@ -1,27 +1,16 @@
 import React from 'react';
+import styles from './styles/Tracks.module.css';
 
 function Tracks() {
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={styles.main}>
+      <h1 className={styles.titleText}>TRACKS</h1>
+      <div className={styles.trackContainer}>
+        <div className={styles.education}>EDUCATION</div>
+      </div>
+      <div className={styles.entertainment}>ENTERTAINMENT</div>
+      <div className={styles.lifehack}>LIFEHACK</div>
+    </main>
   );
 }
 
