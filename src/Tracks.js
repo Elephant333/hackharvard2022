f82b7b73e0d4b573a6c32ccc62fc0202551b7e94
MKJM2 <mailformmk@gmail.com>
2022-07-26 20:30:53 +0200

    Working backfaces for the flipcard on Track subpage

--- src/Tracks.js
+++ src/Tracks.js
@@ -12,6 +12,15 @@ function Tracks() {
           </div>
           <div className={styles.back}>
             The description for the education track goes here!
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare ligula
+            ultricies sapien rutrum, at porta mi vehicula. Duis aliquet enim odio, ac
+            aliquam velit dapibus vitae. Nunc cursus in ipsum aliquam dapibus. Maecenas
+            finibus, massa ac commodo venenatis, lectus eros accumsan ipsum, eu pharetra
+            ante lacus sit amet nulla. Cras libero mauris, eleifend non commodo sed, egestas
+            a erat. Sed maximus, metus et tempus hendrerit, enim nulla dapibus libero, quis
+            ultricies mi diam ut erat. Aenean eleifend dictum leo. Quisque malesuada
+            elementum purus. Quisque sit amet sem at magna vestibulum laoreet quis ut ipsum.
+            Mauris eget tempor velit, ac pellentesque nisi.
           </div>
         </div>
       </div>
