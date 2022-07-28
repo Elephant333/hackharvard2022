3b0a12ac6219851c526f20ef6b6015a93b1850ad
Nathan Li <moncskoli@gmail.com>
2022-07-27 21:02:33 -0400

    added hover to speaker cards

--- src/Speakers.js
+++ src/Speakers.js
@@ -8,10 +8,6 @@ import img1 from './assets/images/1.jpg';
 import img2 from './assets/images/2.jpg';
 import img3 from './assets/images/3.jpg';
 import img4 from './assets/images/4.jpg';
-import img5 from './assets/images/5.jpg';
-import img6 from './assets/images/6.jpg';
-import img7 from './assets/images/7.jpg';
-import img8 from './assets/images/8.jpg';
 
 function Speakers() {
   return (
@@ -74,69 +70,41 @@ function Speakers() {
           slidesToSlide={1}
           swipeable
         >
-          <div className={styles.speakerCard}>
-            <img
-              src={img1}
-              alt="First slide"
-              className={styles.speakerImage}
-            />
+          <div>
+            <div className={styles.speakerContainer}>
+              <img src={img1} alt="First slide" className={styles.speakerImage} />
+              <div className={styles.speakerOverlay}>
+                <div className={styles.speakerText}>Speaker description...</div>
+              </div>
+            </div>
             <p className={styles.speakerName}>SPEAKER 1</p>
           </div>
           <div>
-            <img
-              src={img2}
-              alt="Second slide"
-              className={styles.speakerImage}
-            />
+            <div className={styles.speakerContainer}>
+              <img src={img2} alt="Second slide" className={styles.speakerImage} />
+              <div className={styles.speakerOverlay}>
+                <div className={styles.speakerText}>Speaker description...</div>
+              </div>
+            </div>
             <p className={styles.speakerName}>SPEAKER 2</p>
           </div>
           <div>
-            <img
-              src={img3}
-              alt="Third slide"
-              className={styles.speakerImage}
-            />
+            <div className={styles.speakerContainer}>
+              <img src={img3} alt="Third slide" className={styles.speakerImage} />
+              <div className={styles.speakerOverlay}>
+                <div className={styles.speakerText}>Speaker description...</div>
+              </div>
+            </div>
             <p className={styles.speakerName}>SPEAKER 3</p>
           </div>
           <div>
-            <img
-              src={img4}
-              alt="Fourth slide"
-              className={styles.speakerImage}
-            />
+            <div className={styles.speakerContainer}>
+              <img src={img4} alt="Fourth slide" className={styles.speakerImage} />
+              <div className={styles.speakerOverlay}>
+                <div className={styles.speakerText}>Speaker description...</div>
+              </div>
+            </div>
             <p className={styles.speakerName}>SPEAKER 4</p>
-          </div>
-          <div className={styles.speakerCard}>
-            <img
-              src={img5}
-              alt="Fifth slide"
-              className={styles.speakerImage}
-            />
-            <p className={styles.speakerName}>SPEAKER 5</p>
-          </div>
-          <div>
-            <img
-              src={img6}
-              alt="Sixth slide"
-              className={styles.speakerImage}
-            />
-            <p className={styles.speakerName}>SPEAKER 6</p>
-          </div>
-          <div>
-            <img
-              src={img7}
-              alt="Seventh slide"
-              className={styles.speakerImage}
-            />
-            <p className={styles.speakerName}>SPEAKER 7</p>
-          </div>
-          <div>
-            <img
-              src={img8}
-              alt="Eigth slide"
-              className={styles.speakerImage}
-            />
-            <p className={styles.speakerName}>SPEAKER 8</p>
           </div>
         </Carousel>
       </div>
