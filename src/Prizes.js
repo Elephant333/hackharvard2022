e56b915632c33b72eafb2917830e7f0a308453ba
Nathan Li <moncskoli@gmail.com>
2022-07-29 15:58:39 -0400

    prizes updates

--- src/Prizes.js
+++ src/Prizes.js
@@ -5,6 +5,16 @@ function Prizes() {
   return (
     <main className={styles.main}>
       <h className={styles.titleText}>TRACKS &amp; PRIZES</h>
+      <div className={styles.prizeBox}>
+        {/* eslint-disable max-len */}
+        <div className={styles.prizeTitle}>
+          What are tracks?
+        </div>
+        <div className={styles.prizeDescription}>
+          Tracks are areas of focus that you may choose to hack in. If you submit your project to a track, you are eligible to win the track-specific prize.
+        </div>
+        {/* eslint-enable max-len */}
+      </div>
     </main>
   );
 }
