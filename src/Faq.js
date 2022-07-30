b88f723e7be15344aed42fe07cb3eea36191c7b9
KaiReed <khreed@college.harvard.edu>
2022-07-30 16:48:25 -0400

    faq page grid

--- src/Faq.js
+++ src/Faq.js
@@ -1,25 +1,59 @@
 import React from 'react';
+import styles from './styles/Faq.module.css';
 
 function Faq() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.background}>
+      <div className={styles.wrapper}>
+        <div className={styles.gridDivs}>
+          <p />
+        </div>
+        <div className={styles.gridDivs} id={styles.Faqs}>
+          <h1>hey</h1>
+        </div>
+        <div className={styles.gridDivs} id={styles.Num}>
+          <p>1</p>
+        </div>
+        <div className={styles.gridDivs} id={styles.Header}>
+          <p>TEAMS</p>
+        </div>
+        <div className={styles.gridDivs}>
+          <h1>hey</h1>
+        </div>
+        <div className={styles.gridDivs} id={styles.TeamsInfo}>
+          <h1>hello</h1>
+        </div>
+        <div className={styles.gridDivs} id={styles.Num}>
+          <p>2</p>
+        </div>
+        <div className={styles.gridDivs} id={styles.Header}>
+          <p>GENERAL</p>
+        </div>
+        <div className={styles.gridDivs}>
+          <h1>hello</h1>
+        </div>
+        <div className={styles.gridDivs} id={styles.GeneralInfo}>
+          <h1>hi</h1>
+        </div>
+        <div className={styles.gridDivs} id={styles.Num}>
+          <p>3</p>
+        </div>
+        <div className={styles.gridDivs} id={styles.Header}>
+          <p>LOGISTICS</p>
+        </div>
+        <div className={styles.gridDivs}>
+          <h1>hi</h1>
+        </div>
+        <div className={styles.gridDivs} id={styles.LogisticsInfo}>
+          <h1>hey</h1>
+        </div>
+        <div className={styles.gridDivs}>
+          none
+        </div>
+        <div className={styles.gridDivs} id={styles.footer}>
+          none
+        </div>
+      </div>
     </div>
   );
 }
