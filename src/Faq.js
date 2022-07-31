886379a7bb9e22249924b9e467e3ad1ee4f24759
Nathan Li <moncskoli@gmail.com>
2022-07-30 22:59:32 -0400

    update faq and tracks

--- src/Faq.js
+++ src/Faq.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import styles from './styles/Faq.module.css';
+import teamPic from './assets/images/teamPic.png';
+import statue from './assets/images/johnHarvard.png';
 
 function Faq() {
   return (
-    <div className={styles.background}>
+    <main className={styles.main}>
       <div className={styles.wrapper}>
-        <div className={styles.gridDivs}>
-          <p />
-        </div>
+        {/* eslint-disable max-len */}
+        <div className={styles.gridDivs} />
         <div className={styles.gridDivs} id={styles.Faqs}>
-          <h1>hey</h1>
+          <h1 className={styles.title}>FAQS</h1>
+          <p className={styles.question}>What if my question is not here?</p>
+          <p className={styles.answer}>Please email us at team@hackharvard.io and we’ll get back to you ASAP!</p>
         </div>
         <div className={styles.gridDivs} id={styles.Num}>
           <p>1</p>
@@ -17,11 +20,15 @@ function Faq() {
         <div className={styles.gridDivs} id={styles.Header}>
           <p>TEAMS</p>
         </div>
-        <div className={styles.gridDivs}>
-          <h1>hey</h1>
-        </div>
+        <div className={styles.gridDivs} />
         <div className={styles.gridDivs} id={styles.TeamsInfo}>
-          <h1>hello</h1>
+          <p className={styles.teamsText}>
+            <p className={styles.question}>What if I don’t have a team?</p>
+            <p className={styles.answer}>If you registered as an individual and do not have a team yet, don’t sweat it. There will be several events and communications channels geared towards facilitating team formation and project ideation. We will help you find people to work with!</p>
+            <p className={styles.question}>How do teams work?</p>
+            <p className={styles.answer}>Each team consists of a maximum of four members. You can create a team and designate teammates during registration, or you can register as an individual. All teammates must submit individual applications. Admission decisions will be all-or-none, so either all of your teammates will receive admission or none of your teammates will receive admission.</p>
+          </p>
+          <img src={teamPic} alt="Team" />
         </div>
         <div className={styles.gridDivs} id={styles.Num}>
           <p>2</p>
@@ -29,11 +36,18 @@ function Faq() {
         <div className={styles.gridDivs} id={styles.Header}>
           <p>GENERAL</p>
         </div>
-        <div className={styles.gridDivs}>
-          <h1>hello</h1>
-        </div>
+        <div className={styles.gridDivs} />
         <div className={styles.gridDivs} id={styles.GeneralInfo}>
-          <h1>hi</h1>
+          <p className={styles.generalText}>
+            <p className={styles.question}>What if I’ve never hacked before?</p>
+            <p className={styles.answer}>That’s ok! In fact, you’ve found the perfect place and time to pick up new skills and concepts. There will be mentors, workshops, and tech talks to help ease you through the hackathon experience. Applications are not filtered based on experience. First time hackers are also eligible for the first-time hacker prize!</p>
+            <p className={styles.question}>When is registration?</p>
+            <p className={styles.answer}>Early applications will be due August 29th. Round 2 applications will be due September 19th.</p>
+            <p className={styles.question}>Can I attend?</p>
+            <p className={styles.answer}>As long as you are an undergraduate student at any college in the world and are 18 or older, you are invited to apply to HackHarvard!</p>
+            <p className={styles.question}>I am interested in sponsoring HackHarvard!</p>
+            <p className={styles.answer}>Please email us at sponsors@hackharvard.io and we’ll get back to you ASAP!</p>
+          </p>
         </div>
         <div className={styles.gridDivs} id={styles.Num}>
           <p>3</p>
@@ -41,20 +55,21 @@ function Faq() {
         <div className={styles.gridDivs} id={styles.Header}>
           <p>LOGISTICS</p>
         </div>
-        <div className={styles.gridDivs}>
-          <h1>hi</h1>
-        </div>
+        <div className={styles.gridDivs} />
         <div className={styles.gridDivs} id={styles.LogisticsInfo}>
-          <h1>hey</h1>
+          <p className={styles.logisticsText}>
+            <p className={styles.question}>Will HackHarvard be in-person this year?</p>
+            <p className={styles.answer}>Yes! Although exact details may change depending on Harvard’s COVID-19 guidelines, the plan is to host a fully in-person hackathon at Harvard this year!</p>
+            <p className={styles.question}>How does an in-person hackathon work?</p>
+            <p className={styles.answer}>Located at Harvard’s Student Organization Center at Hilles (SOCH), HackHarvard will provide food for all meals from Friday’s dinner to Sunda’s breakfast. We will try our best to accommodate all dietary restrictions. Hackers will also be permitted to sleep in the SOCH.</p>
+          </p>
+          <img src={statue} alt="Statue" />
         </div>
-        <div className={styles.gridDivs}>
-          none
-        </div>
-        <div className={styles.gridDivs} id={styles.footer}>
-          none
-        </div>
+        <div className={styles.gridDivs} />
+        <div className={styles.gridDivs} id={styles.footer} />
+        {/* eslint-enable max-len */}
       </div>
-    </div>
+    </main>
   );
 }
 
--- src/Tracks.js
+++ src/Tracks.js
@@ -4,6 +4,7 @@ import styles from './styles/Tracks.module.css';
 function Tracks() {
   return (
     <main className={styles.main}>
+      {/* eslint-disable max-len */}
       <h1 className={styles.titleText}>TRACKS</h1>
       <div className={styles.education}>
         <div className={styles.trackContainer}>
@@ -11,16 +12,7 @@ function Tracks() {
             EDUCATION
           </div>
           <div className={styles.back}>
-            The description for the education track goes here!
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare ligula
-            ultricies sapien rutrum, at porta mi vehicula. Duis aliquet enim odio, ac
-            aliquam velit dapibus vitae. Nunc cursus in ipsum aliquam dapibus. Maecenas
-            finibus, massa ac commodo venenatis, lectus eros accumsan ipsum, eu pharetra
-            ante lacus sit amet nulla. Cras libero mauris, eleifend non commodo sed, egestas
-            a erat. Sed maximus, metus et tempus hendrerit, enim nulla dapibus libero, quis
-            ultricies mi diam ut erat. Aenean eleifend dictum leo. Quisque malesuada
-            elementum purus. Quisque sit amet sem at magna vestibulum laoreet quis ut ipsum.
-            Mauris eget tempor velit, ac pellentesque nisi.
+            With the rise of online learning resources like Quizlet, KhanAcademy, Duolingo, and EdX, digital learning has become increasingly important to the contemporary student. Projects in this track may provide fresh perspectives on and increased accessibility of digital education and online learning platforms. Additionally, they may find new ways of teaching, motivating, and retaining students.
           </div>
         </div>
       </div>
@@ -30,7 +22,7 @@ function Tracks() {
             ENTERTAINMENT
           </div>
           <div className={styles.back}>
-            The description for the entertainment track would go here
+            The role of technology in entertainment has grown explosively. From CGI in movies to mobile apps with hours of storytelling to fun online party games, there is no limit to what technology can do for entertainment. Projects in this track will present new “hacks” of entertainment such as games, arts, and apps. Hackers may invent something entirely new or create an altered/advanced version of an existing form of entertainment.
           </div>
         </div>
       </div>
@@ -40,10 +32,11 @@ function Tracks() {
             LIFEHACK
           </div>
           <div className={styles.back}>
-            The description for the lifehack track. Woho!
+            Let’s be casual for a second. We know it and you know it: Life’s hard. Yet, since the dawn of humanity, people have strived to become the best versions of themselves despite this relentless struggle. Some find it through better health, while others find it in heightened productivity. From fitness trackers to sleep trackers to daily schedulers, we want to see how you can improve life through your project. Projects in this track are far-reaching in type and seek to “hack” the daily lifestyles of people, individually or group wide.
           </div>
         </div>
       </div>
+      {/* eslint-enable max-len */}
     </main>
   );
 }
