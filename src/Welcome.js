80c2ca71a9eaefc5fe96b6bb4ae99e4045e51896
Maya Dummett <67915049+mayadummett@users.noreply.github.com>
2022-08-02 23:26:21 -0400

    working on typing animation

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import Typical from 'react';
 import styles from './styles/Welcome.module.css';
 
 function Welcome() {
   return (
     <main className={styles.main}>
       {/* eslint-disable max-len */}
-      <div className={styles.welcomeText}>
+      <p>
+        {' '}
+        Welcome to HackHarvard 2022!
+        <Typical
+          loop={Infinity}
+          wrapper="b"
+          steps={[
+            'Join us for the seventh iteration of HackHarvard, a 36-hour free hackathon hosted by Harvard University students. For the first time in three years, we will be returning to a fully in-person hackathon!',
+            1000,
+            'This year, our theme is Control, Alt, Create. It’s time to get retro and take inspiration from the past: whether that means hearkening to an early technological age, or booting up an old, forgotten project to give it a new, creative twist.',
+            1000,
+          ]}
+        />
+      </p>
+
+      {/* <div className={styles.welcomeText}>
         Welcome to HackHarvard 2022! Join us for the seventh iteration of HackHarvard, a 36-hour free hackathon hosted by Harvard University students. For the first time in three years, we will be returning to a fully in-person hackathon!
         <br />
         <br />
         This year, our theme is Control, Alt, Create. It’s time to get retro and take inspiration from the past: whether that means hearkening to an early technological age, or booting up an old, forgotten project to give it a new, creative twist.
-      </div>
+      </div> */}
       {/* eslint-enable max-len */}
     </main>
   );
