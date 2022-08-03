78201ff24005d840d6a976a3c62dc7b52252da3b
Nathan Li <moncskoli@gmail.com>
2022-08-02 21:17:04 -0400

    prizes more mobile friendly

--- src/Prizes.js
+++ src/Prizes.js
@@ -5,36 +5,41 @@ import trophy from './assets/images/trophy.png';
 function Prizes() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.titleText}>TRACKS &amp; PRIZES</h1>
+      <h1 className={styles.titleText}>
+        TRACKS &amp; PRIZES
+        <img src={trophy} alt="Trophy" className={styles.trophy} id={styles.small} />
+      </h1>
       <div className={styles.prizeBox}>
-        {/* eslint-disable max-len */}
-        <div className={styles.prizeTitle}>
-          What are tracks?
+        <div>
+          {/* eslint-disable max-len */}
+          <div className={styles.prizeTitle}>
+            What are tracks?
+          </div>
+          <div className={styles.prizeDescription}>
+            Tracks are areas of focus that you may choose to hack in. If you submit your project to a track, you are eligible to win the track-specific prize.
+          </div>
+          <div className={styles.prizeTitle}>
+            Do I have to submit a track?
+          </div>
+          <div className={styles.prizeDescription}>
+            No, you can submit a project that is unrelated to the tracks provided.
+          </div>
+          <div className={styles.prizeTitle}>
+            Will there be swag and prizes?
+          </div>
+          <div className={styles.prizeDescription}>
+            Of course! We will be providing swag to all participants who submitted a project, as well as prizes to all winning projects.
+          </div>
+          <div className={styles.prizeTitle}>
+            Do I have to submit a project?
+          </div>
+          <div className={styles.prizeDescription}>
+            You do not have to submit a project, though we highly recommend you do! You will not be eligible to receive swag if you do not submit a project.
+          </div>
         </div>
-        <div className={styles.prizeDescription}>
-          Tracks are areas of focus that you may choose to hack in. If you submit your project to a track, you are eligible to win the track-specific prize.
-        </div>
-        <div className={styles.prizeTitle}>
-          Do I have to submit a track?
-        </div>
-        <div className={styles.prizeDescription}>
-          No, you can submit a project that is unrelated to the tracks provided.
-        </div>
-        <div className={styles.prizeTitle}>
-          Will there be swag and prizes?
-        </div>
-        <div className={styles.prizeDescription}>
-          Of course! We will be providing swag to all participants who submitted a project, as well as prizes to all winning projects.
-        </div>
-        <div className={styles.prizeTitle}>
-          Do I have to submit a project?
-        </div>
-        <div className={styles.prizeDescription}>
-          You do not have to submit a project, though we highly recommend you do! You will not be eligible to receive swag if you do not submit a project.
-        </div>
+        <img src={trophy} alt="Trophy" className={styles.trophy} id={styles.large} />
         {/* eslint-enable max-len */}
       </div>
-      <img src={trophy} alt="Trophy" className={styles.trophy} />
     </main>
   );
 }
