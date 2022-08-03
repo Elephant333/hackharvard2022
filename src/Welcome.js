afbdfba0711d64cde744fffd71e6f250c5209c46
Maya Dummett <67915049+mayadummett@users.noreply.github.com>
2022-08-03 00:42:53 -0400

    changing loop on typing animation

--- src/Welcome.js
+++ src/Welcome.js
@@ -10,7 +10,7 @@ function Welcome() {
         {' '}
         Welcome to HackHarvard 2022!
         <Typical
-          loop={Infinity}
+          loop={1}
           wrapper="p"
           steps={[
             'Join us for the seventh iteration of HackHarvard,',
@@ -31,12 +31,12 @@ function Welcome() {
         />
       </p>
 
-      {/* <div className={styles.welcomeText}>
+      <div className={styles.welcomeText}>
         Welcome to HackHarvard 2022! Join us for the seventh iteration of HackHarvard, a 36-hour free hackathon hosted by Harvard University students. For the first time in three years, we will be returning to a fully in-person hackathon!
         <br />
         <br />
         This year, our theme is Control, Alt, Create. It’s time to get retro and take inspiration from the past: whether that means hearkening to an early technological age, or booting up an old, forgotten project to give it a new, creative twist.
-      </div> */}
+      </div>
       {/* eslint-enable max-len */}
     </main>
   );
