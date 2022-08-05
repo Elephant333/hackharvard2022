8d534023f9168fad4ac942a87b7eb22242772c59
Nathan Li <moncskoli@gmail.com>
2022-08-05 10:53:08 -0400

    footer and code of conduct

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaGithubSquare,
+} from 'react-icons/fa';
 import styles from './styles/Sponsors.module.css';
 
 function Sponsors() {
@@ -12,6 +15,22 @@ function Sponsors() {
         If you would like to sponsor this event, please reach out
         to us at sponsors@hackharvard.io!
       </p>
+      <div className={styles.footer}>
+        <div id={styles.footerTop}>
+          <p id={styles.text}>OCTOBER 14-16 2022</p>
+          <div id={styles.socials}>
+            { /* eslint-disable */ }
+            <a id={styles.icon} href="https://www.facebook.com/hackharvard/" target="_blank" rel="noreferrer"><FaFacebookSquare /></a>
+            <a id={styles.icon} href="https://www.instagram.com/hackharvardcollege/" target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
+            <a id={styles.icon} href="https://www.linkedin.com/company/hackharvardcollege/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+            <a id={styles.icon} href="https://github.com/hackharvard/hackharvard.github.io" target="_blank" rel="noreferrer"><FaGithubSquare /></a>
+            { /* eslint-enable */ }
+          </div>
+          <p id={styles.text}>APPLICATIONS OPEN 8/15!</p>
+        </div>
+        <p id={styles.smallText}>MADE WITH &hearts; FROM HACKHARVARD TEAM</p>
+        <a id={styles.smallText} href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank" rel="noreferrer">MLH CODE OF CONDUCT</a>
+      </div>
     </main>
   );
 }
