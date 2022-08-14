866681b9ebc9f4772a1056ce79ada177f60c6375
Nathan Li <moncskoli@gmail.com>
2022-08-14 11:41:11 -0400

    faq updates

--- src/Faq.js
+++ src/Faq.js
@@ -43,9 +43,13 @@ function Faq() {
             <p className={styles.question}>What if I’ve never hacked before?</p>
             <p className={styles.answer}>That’s ok! In fact, you’ve found the perfect place and time to pick up new skills and concepts. There will be mentors, workshops, and tech talks to help ease you through the hackathon experience. Applications are not filtered based on experience. First time hackers are also eligible for the first-time hacker prize!</p>
             <p className={styles.question}>When is registration?</p>
-            <p className={styles.answer}>Early applications will be due August 29th. Round 2 applications will be due September 19th.</p>
+            <p className={styles.answer}>Early applications will be due September 4th. Round 2 applications will be due September 28th.</p>
             <p className={styles.question}>Can I attend?</p>
             <p className={styles.answer}>As long as you are an undergraduate student at any college in the world and are 18 or older, you are invited to apply to HackHarvard!</p>
+            <p className={styles.question}>Does it cost anything?</p>
+            <p className={styles.answer}>Admission to HackHarvard is 100% free for all participants!</p>
+            <p className={styles.question}>Can I volunteer at HackHarvard?</p>
+            <p className={styles.answer}>Yes! If you are interested in being a mentor, a judge, or something else, please reach out to team@hackharvard.io!</p>
             <p className={styles.question}>I am interested in sponsoring HackHarvard!</p>
             <p className={styles.answer}>Please email us at sponsors@hackharvard.io and we’ll get back to you ASAP!</p>
           </p>
@@ -62,7 +66,9 @@ function Faq() {
             <p className={styles.question}>Will HackHarvard be in-person this year?</p>
             <p className={styles.answer}>Yes! Although exact details may change depending on Harvard’s COVID-19 guidelines, the plan is to host a fully in-person hackathon at Harvard this year!</p>
             <p className={styles.question}>How does an in-person hackathon work?</p>
-            <p className={styles.answer}>Located at Harvard’s Student Organization Center at Hilles (SOCH), HackHarvard will provide food for all meals from Friday’s dinner to Sunda’s breakfast. We will try our best to accommodate all dietary restrictions. Hackers will also be permitted to sleep in the SOCH.</p>
+            <p className={styles.answer}>Located at Harvard’s Student Organization Center at Hilles (SOCH), HackHarvard will provide food for all meals from Friday’s dinner to Sunda’s breakfast. We will try our best to accommodate all dietary restrictions. Hackers will also be provided spaces to sleep in the SOCH.</p>
+            <p className={styles.question}>How do I get to HackHarvard?</p>
+            <p className={styles.answer}>If you’re traveling from outside of Massachusetts, we’ll reimburse up to $30 for your travel as long as you upload your travel receipts upon confirming your attendance and submit a project before judging at the event. Exact reimbursement amounts will be updated closer to the hackathon start date.</p>
             <img src={statue} alt="Statue" className={styles.statue} id={styles.small} />
           </p>
           <img src={statue} alt="Statue" className={styles.statue} id={styles.large} />
