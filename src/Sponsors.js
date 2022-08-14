2177915775b6bca43bde707758c15e53591e84f2
Nathan Li <moncskoli@gmail.com>
2022-08-14 19:48:05 -0400

    add emal icon

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {
   FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaGithubSquare,
 } from 'react-icons/fa';
+import {
+  MdOutlineEmail,
+} from 'react-icons/md';
 import styles from './styles/Sponsors.module.css';
 import aneta from './assets/images/anetaBTC_logo_text_white.png';
 import mlh from './assets/images/mlh-logo-color.png';
@@ -25,6 +28,7 @@ function Sponsors() {
             { /* eslint-disable */ }
             <a id={styles.icon} href="https://www.facebook.com/hackharvard/" target="_blank" rel="noreferrer"><FaFacebookSquare /></a>
             <a id={styles.icon} href="https://www.instagram.com/hackharvardcollege/" target="_blank" rel="noreferrer"><FaInstagramSquare /></a>
+            <a id={styles.icon} href="mailto:team@hackharvard.io" rel="noreferrer"><MdOutlineEmail /></a>
             <a id={styles.icon} href="https://www.linkedin.com/company/hackharvardcollege/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
             <a id={styles.icon} href="https://github.com/hackharvard/hackharvard.github.io" target="_blank" rel="noreferrer"><FaGithubSquare /></a>
             { /* eslint-enable */ }
