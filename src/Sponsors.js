7ced7896e77459e2f95a2229869e0311cd95f454
Nathan Li <moncskoli@gmail.com>
2022-08-23 08:27:21 -0400

    adding locket sponsor

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -11,6 +11,7 @@ import mlh from './assets/images/mlh-logo-color.png';
 import scm from './assets/images/SCM_logo.png';
 import sonr from './assets/images/sonrLogo.png';
 import gCloud from './assets/images/gCloud.png';
+import locket from './assets/images/locket.png';
 
 function Sponsors() {
   return (
@@ -19,6 +20,7 @@ function Sponsors() {
       <div className={styles.sponsorsBox}>
         <a href="https://anetabtc.io/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.aneta}><img className={styles.sponsorLogos} src={aneta} alt="anetaBTC" /></a>
         <a href="https://www.scm-lp.com/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.scm}><img className={styles.sponsorLogos} src={scm} alt="SCM" /></a>
+        <a href="https://apps.apple.com/us/app/locket-widget/id1600525061" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.locket}><img className={styles.sponsorLogos} src={locket} alt="Locketcamera" /></a>
         <a href="https://www.sonr.io/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.sonr}><img className={styles.sponsorLogos} src={sonr} alt="Sonr.io" /></a>
         <a href="https://rsvp.withgoogle.com/events/google-cloud-speaker-series/hackharvard" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.gCloud}><img className={styles.sponsorLogos} src={gCloud} alt="Google Cloud" /></a>
         <a href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=white" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.mlh}><img className={styles.sponsorLogos} src={mlh} alt="MLH" /></a>
