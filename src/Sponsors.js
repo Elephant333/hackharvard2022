9a76f5643a209fb9e357eacd6f2797ef0879e113
Nathan Li <moncskoli@gmail.com>
2022-08-31 14:26:30 -0400

    echo3D and mathpix

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -15,6 +15,8 @@ import locket from './assets/images/locket.png';
 import ll from './assets/images/leadingLearners.png';
 import aa from './assets/images/assemblyai.png';
 import meta from './assets/images/meta.png';
+import echo3d from './assets/images/echo3d.png';
+import mathpix from './assets/images/mathpix.png';
 
 function Sponsors() {
   return (
@@ -22,6 +24,7 @@ function Sponsors() {
       <h1 className={styles.titleText}>SPONSORS</h1>
       <div className={styles.sponsorsBox}>
         <a href="https://about.facebook.com/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.meta}><img className={styles.sponsorLogos} src={meta} alt="Meta" /></a>
+        <a href="https://mathpix.com/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.mathpix}><img className={styles.sponsorLogos} src={mathpix} alt="Mathpix" /></a>
         <a href="https://anetabtc.io/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.aneta}><img className={styles.sponsorLogos} src={aneta} alt="anetaBTC" /></a>
         <a href="https://www.scm-lp.com/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.scm}><img className={styles.sponsorLogos} src={scm} alt="SCM" /></a>
         <a href="https://apps.apple.com/us/app/locket-widget/id1600525061" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.locket}><img className={styles.sponsorLogos} src={locket} alt="Locketcamera" /></a>
@@ -29,6 +32,7 @@ function Sponsors() {
         <a href="https://www.sonr.io/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.sonr}><img className={styles.sponsorLogos} src={sonr} alt="Sonr.io" /></a>
         <a href="https://rsvp.withgoogle.com/events/google-cloud-speaker-series/hackharvard" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.gCloud}><img className={styles.sponsorLogos} src={gCloud} alt="Google Cloud" /></a>
         <a href="https://www.leading-learners.com/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.ll}><img className={styles.sponsorLogos} src={ll} alt="Leading Learners" /></a>
+        <a href="https://www.echo3d.co/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.echo3d}><img className={styles.sponsorLogos} src={echo3d} alt="echo3D" /></a>
         <a href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=white" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.mlh}><img className={styles.sponsorLogos} src={mlh} alt="MLH" /></a>
       </div>
       <p className={styles.sponsorsBottom}>
