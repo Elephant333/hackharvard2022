c39bea66a14fa60a0971b22dde3662e651d76df4
ekang7 <44958279+ekang7@users.noreply.github.com>
2022-09-14 23:17:40 -0400

    Fixed typo of "hearkening" -> "harkening"

--- src/Welcome.js
+++ src/Welcome.js
@@ -22,7 +22,7 @@ function Welcome() {
           loop={1}
           wrapper="p"
           steps={[
-            'This year, our theme is Control, Alt, Create. It’s time to get retro and take inspiration from the past: whether that means hearkening to an early technological age, or booting up an old, forgotten project to give it a new, creative twist.',
+            'This year, our theme is Control, Alt, Create. It’s time to get retro and take inspiration from the past: whether that means harkening to an early technological age, or booting up an old, forgotten project to give it a new, creative twist.',
             0,
           ]}
         />
@@ -32,7 +32,7 @@ function Welcome() {
         Welcome to HackHarvard 2022! Join us for the seventh iteration of HackHarvard, a 36-hour free hackathon hosted by Harvard University students. For the first time in three years, we will be returning to a fully in-person hackathon!
         <br />
         <br />
-        This year, our theme is Control, Alt, Create. It’s time to get retro and take inspiration from the past: whether that means hearkening to an early technological age, or booting up an old, forgotten project to give it a new, creative twist.
+        This year, our theme is Control, Alt, Create. It’s time to get retro and take inspiration from the past: whether that means harkening to an early technological age, or booting up an old, forgotten project to give it a new, creative twist.
       </div> */}
       {/* eslint-enable max-len */}
     </main>
