159a0034c377b876804855b0903fea8c59428ee1
Yuen Ler Chow <yuenlerchow@college.harvard.edu>
2022-09-17 17:57:12 -0400

    stuff

--- src/Schedule.js
+++ src/Schedule.js
@@ -6,7 +6,33 @@ function Schedule() {
     <main className={styles.main}>
       <h1 className={styles.titleText}>SCHEDULE</h1>
       <div className={styles.scheduleBox}>
-        Coming soon...
+        <div>
+          Friday
+          <div className={styles.scheduleText}>
+            <p>Registration: 6:00 PM - 7:00 PM</p>
+            <p>Opening Ceremony: 7:00 PM - 8:00 PM</p>
+            <p>Icebreaker: 8:00 PM - 9:00 PM</p>
+            <p>Free Time: 9:00 PM - 10:00 PM</p>
+          </div>
+        </div>
+        <div>
+          Saturday
+          <div className={styles.scheduleText}>
+            <p>Registration: 6:00 PM - 7:00 PM</p>
+            <p>Opening Ceremony: 7:00 PM - 8:00 PM</p>
+            <p>Icebreaker: 8:00 PM - 9:00 PM</p>
+            <p>Free Time: 9:00 PM - 10:00 PM</p>
+          </div>
+        </div>
+        <div>
+          Sunday
+          <div className={styles.scheduleText}>
+            <p>Registration: 6:00 PM - 7:00 PM</p>
+            <p>Opening Ceremony: 7:00 PM - 8:00 PM</p>
+            <p>Icebreaker: 8:00 PM - 9:00 PM</p>
+            <p>Free Time: 9:00 PM - 10:00 PM</p>
+          </div>
+        </div>
       </div>
     </main>
   );
