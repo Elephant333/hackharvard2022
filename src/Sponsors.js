77ea3b2aab4c94783f25430a8a9e2b55e6a94f43
Nathan Li <moncskoli@gmail.com>
2022-09-21 16:08:26 -0400

    raptor

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -21,6 +21,7 @@ import pi from './assets/images/pi.png';
 import mathworks from './assets/images/mathworks.png';
 import techtreeroot from './assets/images/techtreeroot.png';
 import hrt from './assets/images/HRT.png';
+import raptor from './assets/images/raptor.png';
 
 function Sponsors() {
   return (
@@ -40,6 +41,7 @@ function Sponsors() {
         <a href="https://www.sonr.io/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.sonr}><img className={styles.sponsorLogos} src={sonr} alt="Sonr.io" /></a>
         <a href="https://rsvp.withgoogle.com/events/google-cloud-speaker-series/hackharvard" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.gCloud}><img className={styles.sponsorLogos} src={gCloud} alt="Google Cloud" /></a>
         <a href="https://www.leading-learners.com/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.ll}><img className={styles.sponsorLogos} src={ll} alt="Leading Learners" /></a>
+        <a href="https://raptormaps.com/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.raptor}><img className={styles.sponsorLogos} src={raptor} alt="Raptor Maps" /></a>
         <a href="https://www.echo3d.co/" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.echo3d}><img className={styles.sponsorLogos} src={echo3d} alt="echo3D" /></a>
         <a href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2023-season&utm_content=white" target="_blank" rel="noreferrer" className={styles.sponsorLogos} id={styles.mlh}><img className={styles.sponsorLogos} src={mlh} alt="MLH" /></a>
       </div>
