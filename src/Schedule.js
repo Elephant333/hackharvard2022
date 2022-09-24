ebe900733a40b8b69cc384732924d546ade5ac26
Yuen Ler Chow <yuenlerchow@college.harvard.edu>
2022-09-24 18:06:36 -0400

    change

--- src/Schedule.js
+++ src/Schedule.js
@@ -6,8 +6,7 @@ function Schedule() {
     <main className={styles.main}>
       <h1 className={styles.titleText}>SCHEDULE</h1>
       <div className={styles.scheduleBox}>
-        Coming soon...
-        {/* <div>
+        <div>
           Friday
           <div className={styles.scheduleText}>
             <p>Registration: 6:00 PM - 7:00 PM</p>
@@ -33,7 +32,7 @@ function Schedule() {
             <p>Icebreaker: 8:00 PM - 9:00 PM</p>
             <p>Free Time: 9:00 PM - 10:00 PM</p>
           </div>
-        </div> */}
+        </div>
       </div>
     </main>
   );
