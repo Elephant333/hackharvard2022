2f41264b7f2c786a861a5f533bab582acb52a1de
Maya Dummett <67915049+mayadummett@users.noreply.github.com>
2022-09-24 18:26:08 -0400

    Updates to Friday schedule

--- src/Schedule.js
+++ src/Schedule.js
@@ -9,10 +9,14 @@ function Schedule() {
         <div>
           Friday
           <div className={styles.scheduleText}>
-            <p>Registration: 6:00 PM - 7:00 PM</p>
-            <p>Opening Ceremony: 7:00 PM - 8:00 PM</p>
-            <p>Icebreaker: 8:00 PM - 9:00 PM</p>
-            <p>Free Time: 9:00 PM - 10:00 PM</p>
+            <p>Registration: 4:00 PM - 7:00 PM | Lobby</p>
+            <p>Dinner: 5:00 PM - 7:00 PM | Lobby </p>
+            <p>
+              Opening Ceremony: 7:00 PM - 8:00 PM | Science Center C + Live Stream in First Floor, Hacker Room Left #104
+            </p>
+            <p>Late Dinner: 8:00 PM - 10:00 PM | Lobby</p>
+            <p>Pi Tech Talk: 9:00 PM - 10:00 PM | Basement Cinema</p>
+            <p>Pitch Pit: Find Your Team: 10:00 PM - 10:50 PM | PH Performance Hall</p>
           </div>
         </div>
         <div>
