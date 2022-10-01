ace977f2dc2242dbbbd1f99f6f33f6cb9ccbcb63
Nathan Li <moncskoli@gmail.com>
2022-09-30 23:47:58 -0400

    fixed pi

--- src/Tracks.js
+++ src/Tracks.js
@@ -10,6 +10,8 @@ function Tracks() {
         <div className={styles.trackContainer}>
           <div className={styles.front}>
             BLOCKCHAIN
+            <br />
+            & WEB3
           </div>
           <div className={styles.back}>
             Blockchain is ushering in the next wave of technological advancements and has had widespread impacts on the financial system and innovation. Pi Network, a social cryptocurrency, is creating the blueprint for true digital citizenship in Web3. Develop your app on Pi Network’s easy-to-use platform, and have the opportunity to plug-in to our community of over 35 million engaged users. Successful hackers  in this track will make use of our APIs and other tech; examples include games, art/NFT apps, DeFi apps, multiverse platforms, social apps and more.
