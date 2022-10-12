e2618105196c1f78d817284169748e3a5d6fbe38
Maya Dummett <67915049+mayadummett@users.noreply.github.com>
2022-10-12 00:11:43 -0400

    initial code for highlight pt 2

--- src/Highlight.js
+++ src/Highlight.js
@@ -4,23 +4,25 @@ import project from './src/assests/images/bookProject.png';
 
 export default function Highlight() {
   return (
-    <div className="container">
-      <img
-        src={project}
-        alt="Project"
-        style={{ marginTop: '12%', display: 'inline-block;' }}
-      />
-      <p>
-        Project Education
-        {' '}
-        <br />
-        Created By:
-        {' '}
-        <br />
-        {' '}
-        <br />
-      </p>
-    </div>
+    <main className={styles.main}>
+      <div className="container">
+        <img
+          src={project}
+          alt="Project"
+          style={{ marginTop: '12%', display: 'inline-block;' }}
+        />
+        <p>
+          Project Education
+          {' '}
+          <br />
+          Created By:
+          {' '}
+          <br />
+          {' '}
+          <br />
+        </p>
+      </div>
+    </main>
 
   );
 }
