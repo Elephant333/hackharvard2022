1e031640389879e653a19b57b5079777f4c7bf9b
Nathan Li <moncskoli@gmail.com>
2022-10-13 22:31:22 -0400

    update speaker

--- src/Speakers.js
+++ src/Speakers.js
@@ -152,7 +152,7 @@ function Speakers() {
                   Fatima Taj
                   <br />
                   <br />
-                  Fatima is a graduate from the University of Waterloo, Canada. Post graduation, she&lsquos worked full-time as a Software Developer at DRW, a trading firm and currently works at Yelp as a Software Engineer.
+                  Fatima is a graduate from the University of Waterloo, Canada. Post graduation, she’s worked full-time as a Software Developer at DRW, a trading firm and currently works at Yelp as a Software Engineer. Fatima is very passionate about supporting fellow students in tech and has spoken at over 50 hackathons across North America this year. In addition, she’s also been a panelist at Harvard WeCode, presented at the Women Who Code Connect Event, Black is Tech Conference and Tapia Conference.
                 </div>
               </div>
             </div>
@@ -166,7 +166,7 @@ function Speakers() {
                   Chante Harris
                   <br />
                   <br />
-                  Chante is the Director of Climate Investment and Partnerships for Venture For ClimateTech (V4C) at SecondMuse, where she sources, funds, and scales the best climate tech solutions across the globe by offering them hands-on support until they are ready for their first customers, pilot runs, and investment. Named by GreenBiz as a 30 Under 30, NASDAQ as a Woman to Watch in 2022, Chante is at the helm of driving climate innovation, building sustainable cities, advancing cross-sector collaboration, and tapping into community to launch better solutions and technologies. She is based in New York and sits on the Board of Summit Impact and Power Shift Network.
+                  Chante is the Director of Climate Investment and Partnerships for Venture For ClimateTech (V4C) at SecondMuse, where she sources, funds, and scales the best climate tech solutions across the globe. Named by GreenBiz as a 30 Under 30, Chante is at the helm of driving climate innovation, building sustainable cities, and tapping into community to launch better solutions and technologies. She is based in New York and sits on the Board of Summit Impact and Power Shift Network.
                 </div>
               </div>
             </div>
