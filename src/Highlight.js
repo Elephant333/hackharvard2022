bad15631962eba873ff2ee91e263db19d476c076
dseum22 <dseum22@gmail.com>
2022-10-14 14:36:15 -0400

    Sizing changes

--- src/Highlight.js
+++ src/Highlight.js
@@ -5,28 +5,13 @@ import project from './assets/images/bookProject.png';
 export default function Highlight() {
   return (
     <main className={styles.main}>
-      <div className={styles.mainGrid}>
-        <img
-          src={project}
-          alt="Project"
-          style={{ display: 'inline-block;', width: '100%' }}
-        />
+      <div className={styles.container}>
+        <img className={styles.image} src={project} alt="Project" />
         <div style={{ textAlign: 'center' }}>
-          <p>
-            Project Education
-            {' '}
-            <br />
-            Created By:
-            {' '}
-            <br />
-            {' '}
-            <br />
-          </p>
+          Hello
           <div />
         </div>
       </div>
-
     </main>
-
   );
 }
